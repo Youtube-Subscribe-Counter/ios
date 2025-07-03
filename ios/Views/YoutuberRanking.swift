1ad8987226dcb82df4a3fd0941744ed7be010da9
moonformeli <mgyang95@gmail.com>
2025-07-04 02:24:33 +0900

    feat: add ranking list who are not in the top 3

---
+++ ios/Views/YoutuberRanking.swift
@@ -0,0 +1,18 @@
+//
+//  YoutuberRanking.swift
+//  ios
+//
+//  Created by Munkyu Yang on 7/3/25.
+//
+
+import SwiftUI
+
+struct YoutuberRanking: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YoutuberRanking()
+}
